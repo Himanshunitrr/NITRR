8eb95a626cde67a427124e67902470f2f040048c
Himanshunitrr <himanshumauryacrz@gmail.com>
2020-09-10 18:53:55 +0530

    ...

--- src/store/actions/newPostAction.js
+++ src/store/actions/newPostAction.js
@@ -1,20 +1,5 @@
 export const newPost = (post) => {
-  return (dispatch, getState, { getFirebase, getFirestore }) => {
-    const firestore = getFirestore()
-    
-    var washingtonRef = firestore.collection("NIT Delhi").doc("DC");
-
-    // Set the "capital" field of the city 'DC'
-    return washingtonRef.update({
-      capital: true
-    })
-      .then(function () {
-        console.log("Document successfully updated!");
-      })
-      .catch(function (error) {
-        // The document probably doesn't exist.
-        console.error("Error updating document: ", error);
-      });
-    dispatch({type: "CREATE_POST", post})
+  return {
+    type: "CREATE_POST", post
   }
 }
